50b40ce4ccdf7dcaf8ef456d5ac4d384826dc154
JC <jamcag@proton.me>
2023-08-25 18:26:48 -0400

    Disallow char from dot product

--- dot.cpp
+++ dot.cpp
@@ -7,8 +7,18 @@
 
 using namespace std;
 
+template<typename T>
+struct can_dot {
+  static constexpr bool value = is_arithmetic_v<T> && !is_same_v<T, char>;
+};
+
+template<typename T>
+constexpr bool can_dot_v = can_dot<T>::value;
+
 template<typename T, typename U>
 typename T::value_type dot(T v1, U v2) {
+  static_assert(can_dot_v<typename T::value_type> && can_dot_v<typename U::value_type>,
+    "Vectors must be numeric.");
   const auto n = v1.size();
   typename T::value_type result = 0.0;
   for (int i = 0; i < n; ++i) {
@@ -18,7 +28,7 @@ typename T::value_type dot(T v1, U v2) {
 }
 
 int main() {
-  vector<int> v{1, 2};
-  array<int, 2> w{3, 4};
+  vector<float> v{1.0, 2.0};
+  array<double, 2> w{3.0, 4.0};
   cout << dot(v, w) << "\n";
 }
